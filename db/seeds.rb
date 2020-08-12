f74eb1960ffe7f18041ac6e71fc350557ebba8e1
allenorton <norton.allene@gmail.com>
2020-08-11 20:37:06 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+videoage = Artist.create!(name: "Video Age")
+videoage.songs.create!(title: "Shadow on the Wall")
+videoage.songs.create!(title: "Pop Therapy")
+videoage.songs.create!(title: "Pleasure Line")
+
+prince = Artist.create!(name: "Prince")
+prince.songs.create!(title: "I Wanna Be Your Lover")
+prince.songs.create!(title: "Purple Rain")
+prince.songs.create!(title: "When Doves Cry")
+
